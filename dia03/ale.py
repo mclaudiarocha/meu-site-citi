e43ebf9311ceda9d36560c9468f8046c4075c5ad
mclaudiarocha <124476560+mclaudiarocha@users.noreply.github.com>
2025-04-28 16:05:01 -0300

    Delete dia03 directory

--- dia03/ale.py
+++
@@ -1,6 +0,0 @@
-sabor_bolo = input("Qual o sabor do bolo?")
-
-if sabor_bolo == "coco":
-    print("Ninguem vai para a festa! Ninguem gosta de bolo de coco!")
-else:
-    print("Hummmm, a festa vai bombar! Todo mundo ama bolo de", sabor_bolo)
